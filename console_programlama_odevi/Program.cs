8cb147e0e9727b80a503cd67870a32f0a55b746f
volkan_salli <volkan_salli@hotmail.com>
2021-11-06 22:22:51 +0300

    Add files via upload

---
+++ console_programlama_odevi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi giriniz");
+            string name=Console.ReadLine();
+            Console.WriteLine("Soyisminizi giriniz");
+            string surname=Console.ReadLine();
+            Console.WriteLine("Merhaba " +name +" " +surname);
+        }
+    }
+}
